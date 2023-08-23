7481247349b9f7d957b05c52f12919d23f1d417b
Jonas Bordewick <bordewickjonas@gmail.com>
2023-08-23 13:24:53 +0200

    [RELEASE] HONUA-DASHBOARD-DATABASE: changes for v1.0.1

--- models/models.go
+++ models/models.go
@@ -1,7 +1,7 @@
 package models
 
 type Dashboard struct {
-	ID      string `bson:"_id, omitempty"`
+	ID      string    `bson:"_id, omitempty"`
 	Widgets []*Widget `bson:"widgets, omitempty"`
 }
 
@@ -20,8 +20,9 @@ type Widget struct {
 	Title             string     `bson:"title,omitempty" json:"title,omitempty"`
 	Unit              string     `bson:"unit,omitempty" json:"unit,omitempty"`
 	EntityID          int32      `bson:"entity_id,omitempty" json:"entity_id,omitempty"`
-	SecondaryEntityID int32      `bson:"secondary_entity_id,omitempty", json:"secondary_entity_id,omitempty"`
+	SecondaryEntityID int32      `bson:"secondary_entity_id,omitempty" json:"secondary_entity_id,omitempty"`
 	Subtitle          string     `bson:"subtitle,omitempty" json:"subtitle,omitempty"`
 	SwitchRules       bool       `bson:"switch_rules,omitempty" json:"switch_rules,omitempty"`
+	Expandable        bool       `bson:"expandable,omitempty" json:"expandable,omitempty"`
 	Cards             []*Widget  `bson:"cards,omitempty" json:"cards,omitempty"`
 }
